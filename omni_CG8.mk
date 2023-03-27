a50781fa673ec7e507880c38c755adb35b58f414
TecH PARVEJ <107097395+TECHPARVEJ@users.noreply.github.com>
2023-03-27 17:30:48 +0600

    TECNO SPARK  8 PRO

---
+++ omni_CG8.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit from TECNO-CG8 device
+$(call inherit-product, device/TECNO/KG8/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := KG8
+PRODUCT_NAME := omni_KG8
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := Tecno SPARK 8 PRO
+PRODUCT_MANUFACTURER := TECNO
